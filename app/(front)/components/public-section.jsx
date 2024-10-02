6dd5f33871a90dbebcb08315b8e0411b3cf59e7b
akankshaverm <priyaakanksha0507@gmail.com>
2024-10-02 21:06:00 +0530

    firstcommit

---
+++ app/(front)/components/public-section.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ProductCard } from './product-card';
+
+const PublicSection = () => {
+  return (
+    <div>
+        <ProductCard/>
+      
+    </div>
+  )
+}
+
+export default PublicSection;
--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import PublicSection from './(front)/components/public-section'
+import { CategoriesList } from './(front)/components/categories-list'
 
 const page = () => {
   return (
-    <div>page</div>
-  )
+    <div className='flex flex-1'>
+      <CategoriesList />
+      <PublicSection />
+    </div>
+  );
 }
 
 export default page
