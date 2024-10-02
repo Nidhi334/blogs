5f9ab4d6caec614b4e7533318f9853df78b5714a
ruchikushwah <ruchi741@bgpgmail.com>
2024-10-02 12:53:14 +0530

    commit

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { PublicHeader } from "./(front)/components/public-header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <PublicHeader/>
         {children}
       </body>
     </html>
